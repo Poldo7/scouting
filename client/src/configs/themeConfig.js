756bf025e334221f5783b1cb3bd85767b286b741
Poldo7 <nencionisamuele@live.it>
2023-03-20 12:31:06 +0100

    adding production env

--- client/src/configs/themeConfig.js
+++ client/src/configs/themeConfig.js
@@ -3,45 +3,44 @@
 //Template config options
 const themeConfig = {
   app: {
-    appName: 'Vuexy',
-    appLogoImage: require('@src/assets/images/logo/logo.png').default,
+    appName: "Domini",
+    appLogoImage: require("@src/assets/images/logo/logo.png").default,
     baseUrl: "",
     clientUrl: "",
     serverUrl: "",
   },
   layout: {
     isRTL: false,
-    skin: 'light', // light, dark, bordered, semi-dark
-    routerTransition: 'fadeIn', // fadeIn, fadeInLeft, zoomIn, none or check this for more transition https://animate.style/
-    type: 'vertical', // vertical, horizontal
-    contentWidth: 'boxed', // full, boxed
+    skin: "light", // light, dark, bordered, semi-dark
+    routerTransition: "fadeIn", // fadeIn, fadeInLeft, zoomIn, none or check this for more transition https://animate.style/
+    type: "vertical", // vertical, horizontal
+    contentWidth: "boxed", // full, boxed
     menu: {
       isHidden: true,
-      isCollapsed: false
+      isCollapsed: false,
     },
     navbar: {
       // ? For horizontal menu, navbar type will work for navMenu type
-      type: 'hidden', // static , sticky , floating, hidden
-      backgroundColor: 'white' // BS color options [primary, success, etc]
+      type: "hidden", // static , sticky , floating, hidden
+      backgroundColor: "white", // BS color options [primary, success, etc]
     },
     footer: {
-      type: 'static' // static, sticky, hidden
+      type: "static", // static, sticky, hidden
     },
     customizer: false,
     scrollTop: true, // Enable scroll to top button
-    toastPosition: 'top-right'
-  }
+    toastPosition: "top-right",
+  },
 }
 
 if (process.env.NODE_ENV == "production") {
-  themeConfig.app.baseUrl = ""
-  themeConfig.app.clientUrl = ""
-  themeConfig.app.serverUrl = ""
+  themeConfig.app.baseUrl = "https://main.dpwj0vvx20lw9.amplifyapp.com"
+  themeConfig.app.clientUrl = "https://main.dpwj0vvx20lw9.amplifyapp.com"
+  themeConfig.app.serverUrl = "https://main.d1k69unoheaudx.amplifyapp.com"
 } else {
   themeConfig.app.baseUrl = "http://localhost/scouting/client/"
   themeConfig.app.clientUrl = "https://localhost:3000/"
   themeConfig.app.serverUrl = "http://localhost:9000/"
 }
-
 
 export default themeConfig
